279b132a81d546254c9f154159b5f0265ff7b9d0
Ahmed Youssef <ahmedalaa@windowslive.com>
2017-12-20 12:32:46 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -4,11 +4,14 @@ shinyUI(
   titlePanel(title=div(img(src="dna_icon.png", height = 40, width = 35),'Genome Analyzer'), windowTitle = 'Genome Analyzer'),
   sidebarLayout(
     sidebarPanel(
-      helpText('Compute and visualize statistics for genomes of different organisms'), hr(),
-      selectInput('organism','Organism',choices = db$organism,selectize = F),
+    #  helpText('Compute and visualize statistics for genomes of different organisms'), hr(),
+      selectInput('organism','Organism', selectize = F,
+                  list("Virus" = db$organism[1:2],"Human" = db$organism[3:4], "Bacteria" = db$organism[5:8], "Other" = db$organism[9:11])),
       textInput('acc','Or enter GenBank accession number'),hr(),
       p(strong('Description')),
       uiOutput('descrip'), hr(),
+      uiOutput('stat label'),
+      uiOutput('stat'), hr(),
       tags$div(class="header", checked=NA,
                tags$p("Source code:"),
                tags$a(href="https://github.com/AhmedYoussef95/Genome-Analyzer", "github.com/AhmedYoussef95")
@@ -17,10 +20,10 @@ shinyUI(
       ),
     
     mainPanel(
-      tabsetPanel(type = "tabs",
-                  tabPanel("Base Counts", box(uiOutput("length"),uiOutput('Acount'), uiOutput('Ccount'), uiOutput('Gcount'), uiOutput('Tcount'))),
-                  tabPanel("GC Content", tags$style(HTML(".js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar {background: #EE7600}")), sliderInput('window','Window Size', min = 5, max=500, value = 20,width = "100%"),withSpinner(plotOutput('gc', height = 500),type = getOption("spinner.type", default = 8))),
-                  tabPanel("N-mers", numericInput('nmers','N-mer',value = 2,min = 2,max = 6),DT::dataTableOutput('nmer table'),style = "background-color: #7e8b9a;")
+      tabsetPanel(type = "tabs", id = "tabs",
+                  tabPanel("Base Counts", box(uiOutput("length"),uiOutput('Acount'), uiOutput('Ccount'), uiOutput('Gcount'), uiOutput('Tcount')), box(plotlyOutput('pie'))),
+                  tabPanel("GC Content", tags$style(HTML(".js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar {background: #EE7600}")), sliderInput('window','Number of Windows', min = 5, max=300, value = 20,width = "100%"),withSpinner(plotOutput('gc', height = 500),type = getOption("spinner.type", default = 8))),
+                  tabPanel("N-mers", numericInput('nmers','Number of consecutive nucleotides (N)',value = 2,min = 2,max = 6),DT::dataTableOutput('nmer table'),style = "background-color: #7e8b9a;")
                   )
     )
   )
